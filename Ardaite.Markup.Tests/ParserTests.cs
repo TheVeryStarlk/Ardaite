ade4aee8be9ee95af23f664dfaed661c8c6fbfda
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2022-01-16 00:36:18 +0300

    Create a unit test method for the parser to test if it parses the identifier correctly

--- Ardaite.Markup.Tests/ParserTests.cs
+++ Ardaite.Markup.Tests/ParserTests.cs
@@ -9,6 +9,18 @@ namespace Ardaite.Markup.Tests;
 public class ParserTests
 {
     [Fact]
+    public void IdentifierSource_Outputs_CorrectIdentifier()
+    {
+        var source = new Lexer("(button)").Run();
+
+        var actual = new Parser(source).Run();
+
+        var expected = new TagNode("button", new Dictionary<string, StringNode>(), new List<TagNode>());
+
+        Assert.Equal(expected.Identifier, actual.Identifier);
+    }
+
+    [Fact]
     public void PropertiesSource_Outputs_CorrectProperties()
     {
         var source = new Lexer("(button text=\"button\")").Run();
