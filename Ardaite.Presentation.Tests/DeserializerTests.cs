c16598d938a792062eda1d35e3ffcd4c5ffed7ca
StarlkYT <starlkytminecraft@gmail.com>
2022-02-08 01:03:16 +0300

    Group projects into two folders, and create unit test for the deserializer

---
+++ Ardaite.Presentation.Tests/DeserializerTests.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Ardaite.Markup.Lexing;
+using Ardaite.Markup.Parsing;
+using Ardaite.Presentation.Controls;
+using Xunit;
+
+namespace Ardaite.Presentation.Tests;
+
+public class DeserializerTests
+{
+    [Fact]
+    public void PropertiesSource_Outputs_CorrectControlHierarchy()
+    {
+        var source = "(stack-panel spacing=\"5\" (label text=\"Label\"))";
+
+        var actual = Deserializer.Run(Parser.Run(Lexer.Run(source)));
+
+        var expected = new StackPanelControl(5)
+        {
+            Children = new List<IControl>()
+            {
+                new LabelControl("Label")
+            }
+        };
+
+        var actualStackPanel = ((StackPanelControl) actual).Children[0];
+        var actualLabel = ((LabelControl) actualStackPanel);
+
+        var expectedLabel = (LabelControl) expected.Children[0];
+
+        Assert.Equal(expectedLabel.Text, actualLabel.Text);
+    }
+}
